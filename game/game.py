99672fa086d1c90db0892014fa42c400b6283963
Arsonist406 <ostap06seniv@gmail.com>
2025-01-04 20:18:34 +0200

    1

--- game/game.py
+++ game/game.py
@@ -65,7 +65,7 @@ class Game:
         lib.CombinationSearcher_destroy.argtypes = [ctypes.c_void_p]
         lib.CombinationSearcher_destroy.restype = None
 
-    def found_winner(self, list_of_potential_winners):
+    def found_winner(self, players):
         # Ініціалізація інтерфейсу для взаємодії з класом CombinationSearcher
         self.c_interface_init()
 
@@ -74,129 +74,95 @@ class Game:
         # Створення вказівника на table_cards_array
         table_cards_ptr = ctypes.cast(table_cards_array, ctypes.POINTER(CtypesCard))
 
-        table_comb_searcher = lib.CombinationSearcher_create(table_cards_ptr, len(table_cards_array), None, 0)
-        try:
-            table_comb_bytes = lib.CombinationSearcher_execute(table_comb_searcher)
-            table_comb = table_comb_bytes.decode('utf-8')
-
-            table_comb_score = lib.CombinationSearcher_getCombScore(table_comb_searcher)
-        finally:
-            # Звільнення пам'яті
-            lib.CombinationSearcher_destroy(table_comb_searcher)
-
-        hash_player_comb = {}
-        for player in list_of_potential_winners:
-            # Перетворення list(player.getHand()) в hand_array (ctypes array)
-            hand_array = self.py_list_to_c_array_convert(list(player.getHand()))
+        # Знаходження найзначущих комбінацій гравіцв
+        hash_player_comb_str_int = {}
+        for player in players:
+            hand = list(player.getHand())
+            # Перетворення hand (list) в hand_array (ctypes array)
+            hand_array = self.py_list_to_c_array_convert(hand)
             # Створення вказівника на hand_array
             hand_ptr = ctypes.cast(hand_array, ctypes.POINTER(CtypesCard))
 
-            comb_searcher = lib.CombinationSearcher_create(table_cards_ptr, len(table_cards_array), hand_ptr, len(hand_array))
+            comb_searcher = lib.CombinationSearcher_create(table_cards_ptr, len(table_cards_array),
+                                                           hand_ptr, len(hand_array))
             try:
+                # Знаходження найзначущої комбінації в table_cards + hand
                 player_comb_bytes = lib.CombinationSearcher_execute(comb_searcher)
                 player_comb = player_comb_bytes.decode('utf-8')
 
                 player_comb_score = lib.CombinationSearcher_getCombScore(comb_searcher)
 
-                hash_player_comb[player] = (player_comb, player_comb_score)
+                hash_player_comb_str_int[player] = (player_comb, player_comb_score)
             finally:
                 # Звільнення пам'яті
                 lib.CombinationSearcher_destroy(comb_searcher)
 
-        potential_draw = 0
-        potential_draw_with_need_of_find_high_card = 0
-        player_greatest_comb = []
-        for player, comb in hash_player_comb.items():
-            if comb[0] == "Royal flush":
-                winner = [player]
-                return winner
+        comb_order = {'High': 1, 'Pair': 2, 'Two pair': 3, 'Three of a kind': 4, 'Straight': 5, 'Flush': 6,
+                      'Full house': 7, 'Four of a kind': 8, 'Straight flush': 9, 'Royal flush': 10}
+        hash_player_comb_int_int = {}
+        # Перетворення назви комбінації в номер
+        for player in players:
+            hash_player_comb_int_int[player] = (
+            comb_order[hash_player_comb_str_int[player][0]], hash_player_comb_str_int[player][1])
 
-            if ((table_comb == comb[0] and table_comb_score == comb[1]) and
-               (table_comb == "Straight flush" or table_comb == "Flush" or
-                table_comb == "Straight" or table_comb == "Full house")):
-                potential_draw += 1
-                continue
-            elif ((table_comb == comb[0] and table_comb_score == comb[1]) and
-                 (table_comb == "Four of a kind" or table_comb == "Three of a kind" or
-                  table_comb == "Two pair" or table_comb == "Pair")):
-                potential_draw_with_need_of_find_high_card += 1
-                continue
+        hash_greatest_comb_players = {}
+        for player, comb in hash_player_comb_int_int.items():
 
-            if len(player_greatest_comb) == 0:
-                player_greatest_comb.append((player, comb))
+            if len(hash_greatest_comb_players) == 0:
+                hash_greatest_comb_players[comb] = [player]
             else:
-                if player_greatest_comb[0][1][0] == comb[0] and player_greatest_comb[0][1][1] == comb[1]:
-                    player_greatest_comb.append((player, comb))
-                elif player_greatest_comb[0][1][0] == comb[0] and player_greatest_comb[0][1][1] < comb[1]:
-                    player_greatest_comb.clear()
-                    player_greatest_comb.append((player, comb))
-                elif player_greatest_comb[0][1][0] == comb[0] and player_greatest_comb[0][1][1] > comb[1]:
+                greatest_comb = next(iter(hash_greatest_comb_players))
+
+                if greatest_comb[0] > comb[0]:
                     continue
-                else:
-                    comb_order = {'High': 1, 'Pair': 2, 'Two pair': 3, 'Three of a kind': 4, 'Straight': 5, 'Flush': 6,
-                                  'Full house': 7, 'Four of a kind': 8, 'Straight flush': 9, 'Royal flush': 10}
+                elif greatest_comb[0] < comb[0]:
+                    hash_greatest_comb_players.clear()
+                    hash_greatest_comb_players[comb] = [player]
+                elif greatest_comb[0] == comb[0] and greatest_comb[1] > comb[1]:
+                    continue
+                elif greatest_comb[0] == comb[0] and greatest_comb[1] < comb[1]:
+                    hash_greatest_comb_players.clear()
+                    hash_greatest_comb_players[comb] = [player]
+                elif greatest_comb[0] == comb[0] and greatest_comb[1] == comb[1]:
+                    hash_greatest_comb_players[greatest_comb].append(player)
 
-                    greatest_comb_power = comb_order[player_greatest_comb[0][1][0]]
-                    present_comb_power = comb_order[comb[0]]
+        list_of_winners = []
+        list_of_potential_winners = next(iter(hash_greatest_comb_players.values()))
+        greatest_comb_str = hash_player_comb_str_int[list_of_potential_winners[0]][0]
+        if len(list_of_potential_winners) == 1:
+            list_of_winners = list_of_potential_winners
 
-                    if present_comb_power > greatest_comb_power:
-                        player_greatest_comb.clear()
-                        player_greatest_comb.append((player, comb))
-                    elif present_comb_power < greatest_comb_power:
-                        continue
+        elif (greatest_comb_str == "Straight flush" or greatest_comb_str == "Flush" or
+              greatest_comb_str == "Straight" or greatest_comb_str == "Full house"):
+            list_of_winners = list_of_potential_winners
 
-        # Перевіряємо на ПОВНУ нічию
-        if potential_draw == len(list_of_potential_winners):
-            self.winner_comb = table_comb
-            return list_of_potential_winners
-
-        elif potential_draw_with_need_of_find_high_card == len(list_of_potential_winners):
+        elif (greatest_comb_str == "Four of a kind" or greatest_comb_str == "Three of a kind" or
+              greatest_comb_str == "Two pair" or greatest_comb_str == "Pair" or greatest_comb_str == "High"):
             values_order = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8,
                             '9': 9, '10': 10, 'jack': 11, 'queen': 12, 'king': 13, 'ace': 14}
 
-            player_score = []
+            greatest_hand_score = 0
             for player in list_of_potential_winners:
                 first_card_score = values_order[player.getHand()[0].getValue()]
                 second_card_score = values_order[player.getHand()[1].getValue()]
-                if first_card_score > second_card_score:
-                    player_score.append((player, first_card_score))
-                elif second_card_score > first_card_score:
-                    player_score.append((player, second_card_score))
 
-            player_score = sorted(player_score, key=lambda x: x[1])
-            last_elem = player_score[len(player_score) - 1]
-            list_if_winners = []
-            # Знаходимо гравців з найвагомішою картою
-            for elem in reversed(player_score):
-                if last_elem[1] == elem[1]:
-                    list_if_winners.append(elem[0])
+                if len(list_of_winners) == 0:
+                    list_of_winners.append(player)
+                    greatest_hand_score = first_card_score + second_card_score
 
-            self.winner_comb = table_comb
-            return list_if_winners
+                elif greatest_hand_score > first_card_score + second_card_score:
+                    continue
 
-        else:
-            list_if_winners = []
-            if len(player_greatest_comb) == 1:
-                list_if_winners.append(player_greatest_comb[0][0])
-            else:
-                values_order = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8,
-                                '9': 9, '10': 10, 'jack': 11, 'queen': 12, 'king': 13, 'ace': 14}
+                elif greatest_hand_score < first_card_score + second_card_score:
+                    list_of_winners.clear()
+                    list_of_winners.append(player)
+                    greatest_hand_score = first_card_score + second_card_score
 
-                player_score = []
-                for player, comb in player_greatest_comb:
-                    first_card_score = values_order[player.getHand()[0].getValue()]
-                    second_card_score = values_order[player.getHand()[1].getValue()]
-                    player_score.append((player, first_card_score + second_card_score))
+                elif greatest_hand_score == first_card_score + second_card_score:
+                    list_of_winners.append(player)
 
-                player_score = sorted(player_score, key=lambda x: x[1])
-                last_elem = player_score[len(player_score) - 1]
-                # Знаходимо гравців з найвагомішою картою
-                for elem in reversed(player_score):
-                    if last_elem[1] == elem[1]:
-                        list_if_winners.append(elem[0])
-
-            self.winner_comb = player_greatest_comb[0][1][0]
-            return list_if_winners
+        self.winner_comb = greatest_comb_str
+        return list_of_winners
 
     def command_execute(self, command_name, player):
         if command_name == "Fold":
